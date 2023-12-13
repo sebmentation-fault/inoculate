2b4a9e113d972d0412263f5b7d536d3d33ba526c
Sebastian Kjällgren <sebastian@kjallgren.com>
2023-12-13 11:51:50 +0000

    Fix `TacticExplaination` to show custom messages.

--- inoculate/lib/modules/lesson_snippet/lesson_snippet.dart
+++ inoculate/lib/modules/lesson_snippet/lesson_snippet.dart
@@ -17,10 +17,37 @@ class InformationCard extends StatefulWidget {
 
   It could be worth checking your internet connectivity/firewall settings, as 
   these could prevent `Inoculate` from loading the messages from our server.
+
+  --- 
+
+  # Testing/Debugging:
+
+  The following should all work.
+
+  Lists:
+
+   * item
+   * another item
+   * one more
+  
+  Enumerated lists:
+
+   1. item
+      
+      continuation of item
+
+   2. another item
+   3. one more
+
+   Displaying an image:
+
+   ![Local Image](resource://assets/images/royalty-free-fake-news-image.jpg)
   """;
   static const String exExp = "# Error!";
 
-  const InformationCard({super.key});
+  final String? data;
+
+  const InformationCard({super.key, this.data});
 
   @override
   State<InformationCard> createState() => _InformationCardState();
@@ -28,33 +55,38 @@ class InformationCard extends StatefulWidget {
 
 class _InformationCardState extends State<InformationCard> {
   final int _minScreenSize = 640;
+  late String data;
+
+  @override
+  void initState() {
+    super.initState();
+
+    data = widget.data ?? InformationCard.defaultExplaination;
+  }
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     bool hasNavBar = MediaQuery.of(context).size.width < _minScreenSize;
 
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(16),
-          constraints:
-              BoxConstraints(maxWidth: width - (32.0 + (hasNavBar ? 0 : 80))),
-          child: Card(
-            margin: const EdgeInsets.all(16.0),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SingleChildScrollView(
-                child: MarkdownBody(
-                  data: InformationCard.defaultExplaination,
-                  styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)),
-                ),
-              ),
+    double height = MediaQuery.of(context).size.height - (hasNavBar ? 100 : 60);
+
+    return Container(
+      padding: const EdgeInsets.all(16),
+      constraints: BoxConstraints(
+          maxWidth: width - (32.0 + (hasNavBar ? 0 : 80)), maxHeight: height),
+      child: Card(
+        margin: const EdgeInsets.all(16.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: MarkdownBody(
+              data: data,
+              styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)),
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
--- inoculate/lib/modules/lesson_snippet/tactic_explaination.dart
+++ inoculate/lib/modules/lesson_snippet/tactic_explaination.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:inoculate/modules/lesson_snippet/lesson_snippet.dart';
 
 /// Class that holds information that explains to the user how the diinformation
 /// tactic is used.
-class TacticExplaination extends StatelessWidget {
-  late String? tacticExplaination;
+class TacticExplaination extends StatefulWidget {
+  final String? tacticExplaination;
 
-  TacticExplaination({super.key, this.tacticExplaination});
+  const TacticExplaination({super.key, this.tacticExplaination});
 
   @override
+  State<TacticExplaination> createState() => _TacticExplainationState();
+}
+
+class _TacticExplainationState extends State<TacticExplaination> {
+  @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        InformationCard(),
-        Row(children: [
+        InformationCard(
+          data: widget.tacticExplaination,
+        ),
+        const Row(children: [
           ElevatedButton(onPressed: null, child: Text("Next")),
         ])
       ],
