0424a188c9f372077d229626be27c66b47d1b74e
Sebastian Kjällgren <sebastian@kjallgren.com>
2023-12-13 15:27:27 +0000

    Offload `navigationRoute` to constant

--- inoculate/lib/config/routes/home/home.dart
+++ inoculate/lib/config/routes/home/home.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:inoculate/constants/app_constants.dart';
+import 'package:inoculate/core/states/route_state.dart';
 import 'package:inoculate/modules/home_navigation/navigation_bar.dart';
 import 'package:inoculate/modules/home_navigation/navigation_rail.dart';
+import 'package:inoculate/modules/home_navigation/navigation_routes.dart';
 import 'package:inoculate/modules/lesson_snippet/incorrect_selection.dart';
 import 'package:inoculate/modules/lesson_snippet/option_selection.dart';
 import 'package:inoculate/modules/lesson_snippet/tactic_explaination.dart';
@@ -28,25 +30,19 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    RouteState routeState = Provider.of<RouteState>(context, listen: true);
+
+    // when the route state changes, show a different widget in the body
+
     return Scaffold(
+      drawer: MediaQuery.of(context).size.width >= navigationRailScreenWidth
+          ? buildNavigationRail(context)
+          : null,
       bottomNavigationBar:
           MediaQuery.of(context).size.width < navigationRailScreenWidth
               ? buildBottomNavigationBar(context)
               : null,
-      body: Row(
-        children: [
-          if (MediaQuery.of(context).size.width >= navigationRailScreenWidth)
-            buildNavigationRail(context),
-          //const OptionSelection(
-          //  answerIndex: 0,
-          //),
-          //TacticExplaination(
-          //  tacticExplaination: "# Hello",
-          //),
-          const IncorrectSelection(),
-        ],
-      ),
-      backgroundColor: Theme.of(context).colorScheme.background,
+      body: navigationRoutes[routeState.selectedIndex].widget,
     );
   }
 }
