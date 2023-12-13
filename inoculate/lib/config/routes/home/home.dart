ffe2b767473b0269cceb14ba585507d9ed164676
Sebastian Kjällgren <sebastian@kjallgren.com>
2023-12-13 16:03:03 +0000

    Display error widget if route has none configured

--- inoculate/lib/config/routes/home/home.dart
+++ inoculate/lib/config/routes/home/home.dart
@@ -1,14 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:inoculate/constants/app_constants.dart';
+import 'package:inoculate/core/error/route_not_found.dart';
 import 'package:inoculate/core/states/route_state.dart';
 import 'package:inoculate/modules/home_navigation/navigation_bar.dart';
 import 'package:inoculate/modules/home_navigation/navigation_rail.dart';
 import 'package:inoculate/modules/home_navigation/navigation_routes.dart';
-import 'package:inoculate/modules/lesson_snippet/incorrect_selection.dart';
-import 'package:inoculate/modules/lesson_snippet/option_selection.dart';
-import 'package:inoculate/modules/lesson_snippet/tactic_explaination.dart';
-import 'package:inoculate/utils/services/auth.dart';
 import 'package:provider/provider.dart';
 
 /// The `Home` Widget includes any useful content that should be included in the
@@ -18,8 +14,12 @@ import 'package:provider/provider.dart';
 /// content in this widget will also be shown.
 ///
 /// In the current version, the `Home` widget includes a media query that
-/// displays a Bottom Navigation Bar on small screens (< `x`px) and a
-/// Navigation Rail on larger screens (> `x`px).
+/// displays a `BottomNavigationBar` on small screens (<
+/// `app_contants.navigationRailScreenWidth`px) and a `NavigationRail` on larger
+/// screens (> `app_contants.navigationRailScreenWidth`px).
+///
+/// It depends on the `NavigationRoutes` constant to determine which widget to
+/// show when the `routeState` is notified of a change.
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -32,17 +32,18 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     RouteState routeState = Provider.of<RouteState>(context, listen: true);
 
-    // when the route state changes, show a different widget in the body
-
     return Scaffold(
-      drawer: MediaQuery.of(context).size.width >= navigationRailScreenWidth
-          ? buildNavigationRail(context)
-          : null,
       bottomNavigationBar:
           MediaQuery.of(context).size.width < navigationRailScreenWidth
               ? buildBottomNavigationBar(context)
               : null,
-      body: navigationRoutes[routeState.selectedIndex].widget,
+      body: Row(
+        children: [
+          if (MediaQuery.of(context).size.width >= navigationRailScreenWidth)
+            buildNavigationRail(context),
+          navigationRoutes[routeState.selectedIndex].widget ?? routeNotFound(),
+        ],
+      ),
     );
   }
 }
