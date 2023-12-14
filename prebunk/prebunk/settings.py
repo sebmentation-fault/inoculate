a7d212c2bb33b5e92a454d4393c8576d29780d53
Sebastian Kjällgren <sebastian@kjallgren.com>
2023-12-14 11:12:26 +0000

    Set `SECRET_KEY` to use enviroment variables

--- prebunk/prebunk/settings.py
+++ prebunk/prebunk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-esm(l2-=+_-87d9&v6_@d-1q298bz167x4@=1o3uvof5jty%$c'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# By default, debug will not be enabled.
+DEBUG = os.environ.get('DJANGO_DEBUG', 'FALSE') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'prebunk',
 ]
 
 MIDDLEWARE = [
