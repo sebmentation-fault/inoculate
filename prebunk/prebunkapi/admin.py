49d94ef96a2e1830c0fee0e75277d738e10d6600
Sebastian Kjällgren <sebastian@kjallgren.com>
2024-01-06 12:12:33 +0000

    Register models to be used on admin page

--- prebunk/prebunkapi/admin.py
+++ prebunk/prebunkapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DisinformationTacticModel, LessonModel, OptionSelectionModel, OptionSelectionTacticModel, TacticExplainationModel
+
+admin.site.register(DisinformationTacticModel)
+admin.site.register(LessonModel)
+admin.site.register(TacticExplainationModel)
+admin.site.register(OptionSelectionModel)
+admin.site.register(OptionSelectionTacticModel)
