01f8e3f7d0ed5e364077200ce21d4fa3b9eeffa3
Sebastian Kjällgren <sebastian@kjallgren.com>
2024-03-09 10:14:55 +0000

    Use the backend when not using debug mode

--- inoculate/lib/constants/api_constants.dart
+++ inoculate/lib/constants/api_constants.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/foundation.dart';
 
 String get apiBase {
-  // If testing mode then use HTTP, otherwise,
-  // use HTTPS
-  return kReleaseMode ? "https://localhost:8000" : "http://localhost:8000";
+  // If testing mode then use localhost, otherwise use the backend server
+  return (kReleaseMode || !kDebugMode)
+      ? "http://209.38.160.225:8000"
+      : "http://localhost:8000";
 }
 
 String get prebunkApiBase {
